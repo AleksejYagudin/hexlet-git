1f6aff9da157ade3eac0add313986970e2e42e5d
AleksejYagudin <49141634+AleksejYagudin@users.noreply.github.com>
2022-03-20 10:32:39 +0400

    change index.php

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
-<?php
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-// Файл не включается на прямую
-// Он загрузится автоматически благодаря автозагрузке
-use Hexlet\Php\Runner;
-
-print_r(Runner\run());
+<html>
+<head>
+    <title>PHP Test</title>
+</head>
+<body>
+<?php echo '<p>Hello World</p>'; ?>
+</body>
+</html>
